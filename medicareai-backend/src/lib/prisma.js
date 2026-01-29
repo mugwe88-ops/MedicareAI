563065a3ea2e7257e19c419ddf78c82ad5f1b2cd
mugwe88-ops <mugwe88@gmail.com>
2026-01-29 09:00:22 +0300

    Update prisma.js

--- medicareai-backend/src/lib/prisma.js
+++ medicareai-backend/src/lib/prisma.js
@@ -1,6 +1,1 @@
-import 'dotenv/config';
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg.Pool({ connectionString });
-
-// 2. ADD THIS: Default Export (Fixes the Render SyntaxError)
