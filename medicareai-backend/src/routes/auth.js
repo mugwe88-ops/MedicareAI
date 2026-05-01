842006f89aa38c87ad979f548a2d85a6ca8e6b5b
mugwe88-ops <mugwe88@gmail.com>
2026-05-01 14:14:14 +0000

    update router

--- medicareai-backend/src/routes/auth.js
+++ medicareai-backend/src/routes/auth.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-// CHANGE THIS LINE: Add curly braces around pool
-import { pool } from "../config/db.js"; 
+import pool from "../config/db.js"; // REMOVED curly braces
 import jwt from "jsonwebtoken";
 import { verifyToken } from "../utils/jwt.js";
 
