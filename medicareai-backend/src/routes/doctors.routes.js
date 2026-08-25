c30fef42ee6f06c6da492eab90f3d5d4f8c4c517
mugwe88-ops <mugwe88@gmail.com>
2026-08-25 10:45:00 +0000

    updated medicareai-backend/src/routes/doctors.routes.js

--- medicareai-backend/src/routes/doctors.routes.js
+++ medicareai-backend/src/routes/doctors.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import pool from "../utils/db.js";
-import { authenticateToken } from "../middleware/auth.middleware.js";
+import { authenticateToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
