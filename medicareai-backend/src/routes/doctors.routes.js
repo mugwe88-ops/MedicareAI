7c115f1fbd1345a1e597c911e23367f423d1bf69
mugwe88-ops <mugwe88@gmail.com>
2026-08-25 10:47:51 +0000

    update line 3 of src/routes/doctors.routes.js to point to ./auth.js

--- medicareai-backend/src/routes/doctors.routes.js
+++ medicareai-backend/src/routes/doctors.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import pool from "../utils/db.js";
-import { authenticateToken } from "../middleware/auth.js";
+import { authenticateToken } from "./auth.js";
 
 const router = express.Router();
 
